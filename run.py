6113fe251849e7d9f71ed7f1bacce43ee0989636
Alex Waldron <walter93@hotmail.co.uk>
2020-04-02 16:50:32 +0100

    heroku update

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from heroku import heroku_dash_example
 from plotting_stuff import example_dash
 
 # for running flash
-from plotting_stuff import qtwidge
+# from plotting_stuff import qtwidge
 
 # get app
 # app = heroku_dash_example.run_me()
@@ -13,4 +13,5 @@ app = example_dash.heroku_call()
 server = app.server # if dash
 
 if __name__ == '__main__':
-    qtwidge.run_dash(app)
+    # qtwidge.run_dash(app)
+    app.run_server(debug=True)
